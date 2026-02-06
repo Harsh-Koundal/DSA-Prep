c3da95d0680dcda545d9f8b4d967ad4fc546e264
Harsh-Koundal <aharsh33039@gmail.com>
2026-02-06 09:29:52 -0800

    3Sum

---
+++ Arrays/DSA-Q4.cpp
@@ -0,0 +1,57 @@
+// 3Sum 
+
+/*
+
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+*/
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int>& nums){
+    vector<vector<int>> res;
+    int n = nums.size();
+
+    sort(nums.begin(),nums.end());
+    for(int i=0;i<n;i++){
+        if(i>0 && nums[i]==nums[i-1]) continue;
+        int left = i+1;
+        int right = n-1;
+
+        while(left<right){
+            int sum = nums[i] + nums[left] + nums[right];
+
+            if(sum == 0){
+                res.push_back({nums[i],nums[left],nums[right]});
+
+                while(left < right && nums[left] == nums[left+1]) left++;
+                while(left < right && nums[right] == nums[right-1]) right--;
+
+                left++;
+                right--;
+            }else if(sum < 0){
+                left++;
+            }else{
+                right--;
+            }
+        }
+    }
+    return res;
+}
+
+int main(){
+    vector<int> nums = {-1,0,1,2,-1,-4};
+    vector<vector<int>> res = threeSum(nums);
+
+    for(int i=0;i<res.size();i++){
+        for(int x : res[i]){
+            cout << x << " ";
+        }
+    }
+    return 0;
+}
