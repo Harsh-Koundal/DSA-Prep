421aeccfb348de8c6c12b1c87b9d4ef9164f4964
Harsh-Koundal <aharsh33039@gmail.com>
2026-02-16 07:53:11 -0800

    Reverse Bits

---
+++ Arrays/DSA-Q16.cpp
@@ -0,0 +1,28 @@
+// Reverse Bits
+
+/*
+
+Reverse bits of a given 32 bits signed integer.
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int reverseBits(int n){
+    unsigned int x = (unsigned int)n;
+    unsigned int res = 0;
+
+    for(int i=0;i<32;i++){
+        res = (res << 1) | (x & 1);
+        x >>= 1;
+    }
+
+    return (int) res;
+}
+
+int main(){
+    int n = 43261596;
+    cout << reverseBits(n);
+    return 0;
+}
