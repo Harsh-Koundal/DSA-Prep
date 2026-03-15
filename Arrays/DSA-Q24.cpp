cc0b08a8aad2939ec52e8b0f2b396737d1031fac
Harsh-Koundal <aharsh3039@gmail.com>
2026-03-15 06:45:06 -0400

    square pattern

---
+++ Arrays/DSA-Q24.cpp
@@ -0,0 +1,28 @@
+// print square pattern 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // square pattern 
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+//  Output
+
+/*
+
+ *****
+ *****
+ *****
+ *****
+ *****
+
+*/
