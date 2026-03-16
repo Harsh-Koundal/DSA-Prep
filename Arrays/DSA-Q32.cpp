6ca6468d1cc2c574625cd18bad406ed15f9ce174
Harsh-Koundal <aharsh3039@gmail.com>
2026-03-15 23:34:25 -0400

    triangle

---
+++ Arrays/DSA-Q32.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int starpattern1(int n){
+    for(int i=0;i<5;i++){
+        for(int j=0;j<2*i+1;j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int starpattern2(int n){
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<2*n-(2*i-1);j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int n = 5;
+    starpattern1(n);
+    starpattern2(n);
+}
+
+// Output 
+
+/*
+
+*
+***
+*****
+*******
+*********
+***********
+*********
+*******
+*****
+***
+*   
+
+*/
