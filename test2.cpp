89dd573d8aa3965e2b8669f8aa2955deb1a9cea6
Harsh-Koundal <aharsh3039@gmail.com>
2026-03-27 10:26:08 -0400

    dsa

--- test2.cpp
+++ test2.cpp
@@ -3,6 +3,7 @@
 #include<unordered_map>
 #include<stack>
 #include<cmath>
+#include<algorithm>
 using namespace std;
 
 vector<int> twoSum(vector<int>& nums,int target){
@@ -127,9 +128,34 @@ int binarySearch(vector<int>& nums, int target){
     return -1; 
 }
 
+int search(vector<int>& nums, int target){
+    int left = 0;
+    int right = nums.size()-1;
+
+    while(left<=right){
+        int mid = left + (right-left)/2;
+
+        if(nums[mid]==target)
+        return mid;
+
+        // left half
+        if(nums[left] < nums[mid]){
+            if(nums[left] <= target && target < nums[mid])
+            right = mid - 1;
+            else left = mid + 1;
+        }
+        // right half
+        else{
+            if(nums[mid] < target && target<=nums[right])
+            left = mid + 1;
+            else right = mid - 1;
+        }
+    }
+}
+
 int main(){
-    vector<int> nums = {1,2,4,6,9,12,34,65,87};
-    int target = 9;
+    vector<int> nums = {4,5,6,7,0,1,2};
+    int target = 0;
     for(int x : twoSum(nums,target))
     cout << x << " ";
     cout << endl;
@@ -149,5 +175,15 @@ int main(){
     cout << findMissing(nums) << endl;
 
     cout << binarySearch(nums,target) << endl;
+
+    cout << search(nums,target) <<endl;
+
+    sort(nums.begin(),nums.end(),[](int a, int b){
+        return a>b;
+    });
+    reverse(nums.begin(),nums.end());
+    for(int x : nums)
+    cout << x << " ";
+
     return 0;
 }
