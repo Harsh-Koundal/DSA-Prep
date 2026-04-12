8ac7c861bd708974ef99deba25394644f34c406c
Harsh-Koundal <aharsh3039@gmail.com>
2026-04-11 23:30:16 -0400

    Two Sum

--- indexss.cpp
+++ indexss.cpp
@@ -7,12 +7,12 @@ using namespace std;
 vector<int> twoSum(vector<int>& nums,int target){
     unordered_map<int,int> map;
     for(int i=0;i<nums.size();i++){
-        int diff = nums[i] - target;
+        int diff = target - nums[i];
 
         if(map.find(diff) != map.end()){
             return  {map[diff],i};
         }
-        map[nums[diff],i] = i;
+        map[nums[i]] = i;
     }
     return {};
 }
