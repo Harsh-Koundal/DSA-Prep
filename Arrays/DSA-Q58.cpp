c1156e5b49c98381cf1c39d60a1adc56234a30c7
Harsh-Koundal <aharsh3039@gmail.com>
2026-04-14 00:50:11 -0400

    Valid Palindrome

---
+++ Arrays/DSA-Q58.cpp
@@ -0,0 +1,45 @@
+// Valid Palindrome
+
+
+/*
+
+Given a string s, return true if it is a palindrome, otherwise return false.
+
+A palindrome is a string that reads the same forward and backward. It is also case-insensitive and ignores all non-alphanumeric characters.
+
+Note: Alphanumeric characters consist of letters (A-Z, a-z) and numbers (0-9).
+
+*/
+
+
+/*
+
+Input: s = "Was it a car or a cat I saw?"
+
+Output: true
+
+*/
+
+
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left =0;
+        int right = s.length()-1;
+
+        while(left<right){
+            while(left<right && !isalnum(s[left])) left++;
+            while(left<right && !isalnum(s[right])) right--;
+
+            if(tolower(s[left]) != tolower(s[right]))
+            return false;
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
